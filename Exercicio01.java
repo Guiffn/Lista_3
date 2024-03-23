9f0340acd37f90a74e3c2a69b79bdc27b174fec4
Guilherme <guiffn2005@gmail.com>
2024-03-23 15:29:42 -0300

    Exercicio01

---
+++ Exercicio01.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Exercicio01 {
+    public static void executar(){
+     Scanner leitor = new Scanner(System.in);
+
+     System.out.println("Digite o nome do aluno");
+     String nome=leitor.nextLine();
+     System.out.println("Digite a nota do aluno");
+     double nota1=leitor.nextDouble();
+     System.out.println("Digite a nota do aluno");
+     double nota2=leitor.nextDouble();
+     System.out.println("Digite a nota do aluno");
+     double nota3=leitor.nextDouble();
+      
+     Aluno01 aluno= new Aluno01( nome, nota1,nota2, nota3);
+
+     double media= aluno.calcularMedia();
+  
+     String nomeAluno= aluno.getNome();
+
+     System.out.println("O aluno: "+nomeAluno+" teve a media igual: "+media);
+
+
+
+
+
+
+
+    leitor.close();}
+}
