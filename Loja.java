80b1fe05d75d6dacb6d0f600b669eee707b2e257
Guilherme <guiffn2005@gmail.com>
2024-03-24 13:54:25 -0300

    Exercicio05

---
+++ Loja.java
@@ -0,0 +1,16 @@
+public class Loja {
+private Double produto;
+
+
+
+public Loja(double produto){
+    this.produto=produto;
+} 
+
+public double calculaPrestação(){
+    return produto/5;
+
+ }
+}
+
+
--- Programa.java
+++ Programa.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
    //Exercicio01.executar();   
    //Exercicio02.executar();
    //Exercicio03.executar();
-    Exercicio04.executar();
- }
+   //Exercicio04.executar();
+    Exercicio05.executar();
+  }
 }
