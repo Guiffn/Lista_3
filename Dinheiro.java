82b2489ca6d32e4e15548e24feecb37f7b2ed36e
Guilherme <guiffn2005@gmail.com>
2024-03-24 13:38:04 -0300

    Exercicio04

---
+++ Dinheiro.java
@@ -0,0 +1,14 @@
+public class Dinheiro {
+private double cotação;
+private double reais;
+
+public Dinheiro(double cotação,double reais){
+    this.cotação=cotação;
+    this.reais=reais;
+}
+
+ public double calcularDolar(){
+    return reais/cotação;
+ }
+
+}
