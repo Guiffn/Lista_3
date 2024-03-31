5fd2ec961a0a6f48588e199e86767d36bd3466a4
Guilherme <guiffn2005@gmail.com>
2024-03-31 00:58:22 -0300

    Exercicio23

---
+++ Exercicio23.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exercicio23{
+    public static void executar(){
+     Scanner leitor = new Scanner(System.in);
+     Ideal Ideal=new Ideal();
+
+      System.out.println("Digite o nome:");
+      String nome=leitor.nextLine();
+      System.out.println("Digite o sexo: (M/H)");
+      String sexo=leitor.nextLine();
+      System.out.println("Digite idade:");
+      int idade=leitor.nextInt();
+      System.out.println("Digite a altura:");
+      double altura=leitor.nextDouble();
+        Ideal.calcularIdeal(nome, sexo, altura, idade);
+      
+     
+leitor.close(); } 
+}
